90685748af12faafa0f09c3de27b607f85c33548
vincent san juan <vsanjuaniii@gmail.com>
2017-05-09 02:09:20 -0700

    Just testing to see if it links with editor

--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//this is a test
