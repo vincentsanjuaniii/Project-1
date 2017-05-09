317939ae78023da59f5300351c3f11fe187f96cb
vincent san juan <vsanjuaniii@gmail.com>
2017-05-09 14:31:12 -0700

    Creating of 'quotes' array
    Has quote, author, and some have citation and year

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 //document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 //will add later once other codes have been updated.
+
+var quotes = [
+	["I think the worst time to have a heart attack is during a game of charades.", "Demetri Martin",],
+	["I love lamp.", "Steve Carell", "Anchor Man"],
+	["Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", "Albert Schweitzer"],
+	["The true measure of an individual is how he treats a person who can do him absolutely no good", "Ann Landers"],
+	["Whenever you read a good book, it's like the author is right there, in the
+		room, talking to you, which is why I don't like to read good books, "Jack Handey", "Saturday Night Live", "1995"],
+	["I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN
+		TO YOURSELF!", "Jack Handey","Saturday Night Live", "1995"]
+];
