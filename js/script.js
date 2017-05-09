4a40af151c96d130498fcf7c9776a148161a7025
vincent san juan <vsanjuaniii@gmail.com>
2017-05-09 14:32:39 -0700

    Missing quotation marks on quote

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ var quotes = [
 	["Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", "Albert Schweitzer"],
 	["The true measure of an individual is how he treats a person who can do him absolutely no good", "Ann Landers"],
 	["Whenever you read a good book, it's like the author is right there, in the
-		room, talking to you, which is why I don't like to read good books, "Jack Handey", "Saturday Night Live", "1995"],
+		room, talking to you, which is why I don't like to read good books", "Jack Handey", "Saturday Night Live", "1995"],
 	["I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN
 		TO YOURSELF!", "Jack Handey","Saturday Night Live", "1995"]
 ];
