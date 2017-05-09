da7b79b259f43b7348b4585e9acebf1d4e169be1
vincent san juan <vsanjuaniii@gmail.com>
2017-05-09 14:01:12 -0700

    temporarily remove load quote code
    will add it back once the other steps have been completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//will add later once other codes have been updated.
