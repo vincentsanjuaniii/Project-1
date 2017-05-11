307a4d25e5913e9083f97fb54eccbf56bee7fbc4
vincent san juan <vsanjuaniii@gmail.com>
2017-05-10 20:16:09 -0700

    Adding objects in array
    Used keys to separate quote, author, and if applicable citation and year

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@
 //will add later once other codes have been updated.
 
 var quotes = [
-	["I think the worst time to have a heart attack is during a game of charades.", "Demetri Martin",],
-	["I love lamp.", "Steve Carell", "Anchor Man"],
-	["Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", "Albert Schweitzer"],
-	["The true measure of an individual is how he treats a person who can do him absolutely no good", "Ann Landers"],
-	["Whenever you read a good book, it's like the author is right there, in the
-		room, talking to you, which is why I don't like to read good books", "Jack Handey", "Saturday Night Live", "1995"],
-	["I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN
-		TO YOURSELF!", "Jack Handey","Saturday Night Live", "1995"]
+	{quote: "I think the worst time to have a heart attack is during a game of charades.", author: "Demetri Martin"},
+	{quote: "I love lamp.", author: "Steve Carell", citation: "Anchor Man"},
+	{quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", author: "Albert Schweitzer"},
+	{quote: "The true measure of an individual is how he treats a person who can do him absolutely no good", author: "Ann Landers"},
+	{quote: "Whenever you read a good book, it's like the author is right there, in the
+		room, talking to you, which is why I don't like to read good books", author: "Jack Handey", citation: "Saturday Night Live", year: "1995"},
+	{quote: "I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN
+		TO YOURSELF!", author: "Jack Handey", citation: "Saturday Night Live", year: "1995"}
 ];
