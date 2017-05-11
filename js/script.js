9ac0a40ff70d8edce2286c9a5d3e19dd877b9fa0
vincent san juan <vsanjuaniii@gmail.com>
2017-05-10 20:50:48 -0700

    format of objects in array and began get quote function
    added citation and year as keys and added a for loop

--- js/script.js
+++ js/script.js
@@ -5,17 +5,42 @@
 
 //quotes where JS selects at random
 var quotes = [
-	{quote: "I think the worst time to have a heart attack is during a game of charades.", author: "Demetri Martin"},
-	{quote: "I love lamp.", author: "Steve Carell", citation: "Anchor Man"},
-	{quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", author: "Albert Schweitzer"},
-	{quote: "The true measure of an individual is how he treats a person who can do him absolutely no good", author: "Ann Landers"},
-	{quote: "Whenever you read a good book, it's like the author is right there, in the
-		room, talking to you, which is why I don't like to read good books", author: "Jack Handey", citation: "Saturday Night Live", year: "1995"},
-	{quote: "I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN
-		TO YOURSELF!", author: "Jack Handey", citation: "Saturday Night Live", year: "1995"}
+	{quote: "I think the worst time to have a heart attack is during a game of charades.", 
+	author: "Demetri Martin",
+	citation: '',
+	year: ''
+	},
+	{quote: "I love lamp.", 
+	author: "Steve Carell", 
+	citation: "Anchor Man",
+	year: ''
+	},
+	{quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful", 
+	author: "Albert Schweitzer",
+	citation:'',
+	year:''
+	},
+	{quote: "The true measure of an individual is how he treats a person who can do him absolutely no good", 
+	author: "Ann Landers",
+	citation: '',
+	year: '',
+	},
+	{quote: "Whenever you read a good book, it's like the author is right there, in the room, talking to you, which is why I don't like to read good books", 
+	author: "Jack Handey", 
+	citation: "Saturday Night Live", 
+	year: "1995"
+	},
+	{quote: "I bet the sparrow looks at the parrot and thinks, yes, you can talk, but LISTEN TO YOURSELF!", 
+	author: "Jack Handey", 
+	citation: "Saturday Night Live", 
+	year: "1995"
+	}
 ];
 
 // create code to randomly select quotes
 function getRandomQuote () {
-
+	for (var i = 0, i<quotes.length; i+=1) {
+		quote = quotes[i];
+		message
+	}
 };
